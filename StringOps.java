f9b60a39fd481f1e35510fb606b5aaa704b9f901
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2024-01-08 22:51:39 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -26,17 +26,97 @@ public class StringOps {
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String str = "";
+        String vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = string.charAt(i);
+            if (vowels.indexOf(currentChar) != -1) {
+                str += (char) (currentChar - 'a' + 'A');
+            } else {
+                str += (char) (currentChar + 'a' - 'A');
+            }
+        }
+
+        return str;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        string = lowercase(string);
+
+        if (string.length() == 1) {
+            return lowercase(string);
+        }
+
+        String newStr = "";
+        boolean first = true;
+
+        for (int i = 0; i < string.length() - 1; i++) {
+            if (string.charAt(i) != ' ') {
+                int end = i + 1;
+
+                if (end < string.length()) {
+                    while (end < string.length() && string.charAt(end) != ' ') {
+                        end++;
+                        if (end == string.length()) {
+                            break;
+                        }
+                    }
+
+                    if (first) {
+                        newStr += string.substring(i, end);
+                        first = false;
+                    } else {
+                        newStr += capFirst(string.substring(i, end));
+                    }
+
+                    i = end - 1; // Adjusting the index to the end of the current word
+                }
+            }
+        }
+
+        return newStr;
     }
+
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int counter = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                counter++;
+            }
+        }
+        int[] times = new int[counter];
+        int index = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                times[index++] = i;
+            }
+        }
+        return times;
+    }
+
+
+public static String lowercase(String string) {
+        String str = "";
+        for(int i = 0; i < string.length(); i++){
+            char ch = string.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'){
+                ch += 32;
+                str += (char)ch;
+            }else{
+                str += (char)ch;
+            }
+        }
+        return str;
+    }
+
+    public static String capFirst(String string) {
+        String newSt = "";
+        if (!(string.charAt(0) >= 'A' && string.charAt(0) <= 'Z')) {
+            newSt += (char) (string.charAt(0) - 32) + string.substring(1);
+        }
+        return newSt;
     }
 }
