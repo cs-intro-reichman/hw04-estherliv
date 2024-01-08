486d8e9f618f1d664d6562d84af30e2411cb5733
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2024-01-08 22:10:02 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -4,23 +4,96 @@ public class ArrayOps {
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int n = array.length + 1;
+        int expectedSum = 0;
+        for (int i = 0; i < n; i++) {
+            expectedSum += i;
+        }
+        
+        int actualSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max = array[0];
+        int secondMax = array[1];
+
+        if (secondMax > max) {
+            int temp = max;
+            max = secondMax;
+            secondMax = temp;
+        }
+
+        for (int i = 2; i < array.length; i++) {
+            int num = array[i];
+            if (num > max) {
+                secondMax = max;
+                max = num;
+            
+            } else if (num > secondMax) {
+                secondMax = num;
+            }
+        }
+
+        return secondMax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        if (array1.length != array2.length) {
+            return false;
+        }
+
+        int[] countArray1 = new int[1001]; // Assuming integers in the range 0 to 1000
+        int[] countArray2 = new int[1001];
+
+        // Count the occurrences in array1
+        for (int i = 0; i < arr1.length; i++) {
+            int num = arr1[i];
+            countArr[num]++;
+        }
+
+        // Decrease according to the occurrences in array2
+        for (int i = 0; i < arr2.length; i++) {
+            int num = arr2[i];
+            countArr[num]--;
+        }
+
+        // Check if all counts are zero
+        for (int i = 0; i < countArr.length; i++) {
+            if (countArr[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        // Check for increasing order
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[i - 1]) {
+                increasing = false;
+                break;
+            }
+        }
+
+        // Check for decreasing order
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[i - 1]) {
+                decreasing = false;
+                break;
+            }
+        }
+
+        return (increasing || decreasing);
     }
 
 }
